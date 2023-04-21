be9b940555f3b1d19887be425327cf06002cde77
신용진 <tlsdydwls123@gmail.com>
2023-04-21 20:40:52 +0900

    FEAT : 스프링부트 자동 등록 예시 및 기반 예제 Member 작성

---
+++ start/autoconfig/src/main/java/hello/member/Member.java
@@ -0,0 +1,15 @@
+package hello.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+
+    private String memberId;
+    private String name;
+
+}
