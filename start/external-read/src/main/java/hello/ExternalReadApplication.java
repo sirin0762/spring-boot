011c8b4c7010cf77a7f061c9efc22dd7685a7b5b
신용진 <tlsdydwls123@gmail.com>
2023-04-24 20:23:46 +0900

    FEAT : @ConfigurationProperties

--- start/external-read/src/main/java/hello/ExternalReadApplication.java
+++ start/external-read/src/main/java/hello/ExternalReadApplication.java
@@ -1,12 +1,17 @@
 package hello;
 
 import hello.config.MyDataSourceConfig;
+import hello.config.MyDataSourceConfigV1;
 import hello.config.MyDataSourceValueConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Import;
 
-@Import(MyDataSourceValueConfig.class)
+@Import(MyDataSourceConfigV1.class)
+//@Import(MyDataSourceValueConfig.class)
+@ConfigurationPropertiesScan(basePackages = {"hello.datasource"})
 @SpringBootApplication(scanBasePackages = {"hello.datasource"})
 public class ExternalReadApplication {
 
